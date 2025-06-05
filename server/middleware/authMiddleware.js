5a78c5f768730b7342d5697e679853f64630e58c
rifahb <138552632+rifahb@users.noreply.github.com>
2025-06-06 03:55:55 +0530

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const protect = (req, res, next) => {
-  const token = req.header('Authorization');
+  let token = req.header('Authorization');
   if (!token) {
     return res.status(401).json({ message: 'Not authorized, no token' });
   }
